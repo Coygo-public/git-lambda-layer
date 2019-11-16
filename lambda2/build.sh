e60605b4cd31567c4abf87570265882330cc9d55
dorian-coygo <43051517+dorian-coygo@users.noreply.github.com>
2019-11-16 14:39:10 -0800

    Add curl 7.67.0 to build

--- lambda2/build.sh
+++ lambda2/build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 export GIT_VERSION=2.24.0
+export CURL_VERSION=7.67.0
 
 rm layer.zip
 
 docker run --rm -v "$PWD":/tmp/layer lambci/yumda:2 bash -c "
-  yum install -y git-${GIT_VERSION} && \
+  yum install -y git-${GIT_VERSION} curl-${CURL_VERSION} && \
   cd /lambda/opt && \
   zip -yr /tmp/layer/layer.zip .
 "
